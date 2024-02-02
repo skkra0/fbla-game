40a569d959da273a375c744fa95b5666effdf0b4
skkra <skkra45@gmail.com>
2024-02-01 23:03:03 -0500

    animation

--- Characters/player_del.gd
+++ Characters/player_del.gd
@@ -7,7 +7,17 @@ func _physics_process(_delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	
+	var animated_sprite = $AnimatedSprite2D
+	if input_direction.x > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	elif input_direction.x < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.stop()
+		animated_sprite.frame = 1
+		
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
