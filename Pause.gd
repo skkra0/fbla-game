ddab3a94958d44d9eab7d55e5764e686040ffdd9
skkra <skkra45@gmail.com>
2024-02-13 22:38:23 -0500

    pause

---
+++ Pause.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = !get_tree().paused
+		$RichTextLabel.visible = !$RichTextLabel.visible
+		
+	if get_tree().paused and Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
